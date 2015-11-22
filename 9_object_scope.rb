3660793922ef27396565ebccf680d1fd09ddf782
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-11-23 00:49:57 +0100

    tiny example of object scope

---
+++ 9_object_scope.rb
@@ -0,0 +1,30 @@
+#################################################################################
+# Tutorial: http://ruby-for-beginners.rubymonstas.org/writing_classes/self.html #
+#################################################################################
+
+class Person
+	def initialize(name)
+		@name = name
+	end
+
+	def name
+		@name
+	end
+
+	def greet(other_person)
+    #This local variable shadows the method 'name'
+    name = "Not my name"
+    # TODO: We have a name clash betweeen a local variable and a method name
+    # TODO: How can we be specific?
+    # TODO :How can we call a method on the current object?
+		puts "Hi " + other_person.name + "! My name is " + name + "."
+	end
+end
+
+person = Person.new("Alice")
+friend = Person.new("Bob")
+person.greet(friend)
+# What do we have to write in order to let Alice tell her name to her friend Bob?
+# Or vice verso?
+friend.greet(person)
+
