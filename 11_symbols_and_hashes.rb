39e32913fc147a3aa85482991d9ce0a70a4242ad
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-11-28 17:11:25 +0100

    finished hash/symbol exercise

--- 11_symbols_and_hashes.rb
+++ 11_symbols_and_hashes.rb
@@ -7,6 +7,8 @@
 # We have skipped over two very relevant basic data types in ruby:
 # Symbols and Hashes
 
+# ============= Symbols ===============
+
 # Symbols are a very specific concept in ruby
 # Their purpose is to label things
 # A symbol is written like a word preceded by a colon:
@@ -34,10 +36,114 @@ puts :word.equal?(:word)
 # Use symbols if you want to talk about "Who?" instead of "What"?
 # 3. Lengthy explanation including implementation details:
 # http://stackoverflow.com/q/16621073
+puts # ------------ print empty line -------------
 
-
+# ============= Hashes ===============
 
 # A 'Hash' in ruby is a set of key-value pairs - a special data structure
 # In other languages it may have the name 'Dictionary' or 'Map'
 # Wikipedia calls it 'associative array'
 # Wikipedia: https://en.wikipedia.org/wiki/Associative_array
+
+# A Hash is written with curly braces
+a_hash = {"I am" => "a hash"}
+# Here the String "I am" is the key for the String "a hash" 
+
+
+# Because a Hash is also called dictionary in programming languages like Java
+# We can make an actual english dictionary with more than one key-value pair
+english_dictionary = {
+  "hello" => "Hallo",
+  "goodbye" => "Auf Wiedersehen",
+  "thanks" => "Danke"
+}
+# Everything between '{' and '}' belongs to the hash
+
+# We can add more key-value pairs to the hash like so:
+english_dictionary["sorry"] = "Entschuldigung"
+# And we can retrieve words from a hash like this:
+puts "return value of 'english_dictionary[\"thanks\"] is"
+puts english_dictionary["thanks"]
+puts # ------------ print empty line -------------
+
+# The whole point about hashes is that you don't need to remember an element
+# in a collection by it's position (like in a list) but you can remember the
+# element with a so called 'key'
+
+# E.g consider this example with two lists
+english_words = ["hello", "goodbye", "thanks"]
+german_words = ["Hallo", "Danke", "Auf Wiedersehen"]
+# We made a little mistake here: The order of corresponding words doesn't match
+
+puts "There is a mistake in the following translations"
+english_words.each_with_index do |word, index|
+  puts "The german translation of '#{word}' is : #{german_words[index]}"
+end
+# TODO: change the code above, so that the right translation appears on the screen
+# Do you see that this is a possible source for errors?
+puts # ------------ print empty line -------------
+
+
+
+# TODO: Let's consider this example
+sentence = "Please translate this sentence"
+# TODO: Add more key-value pairs to the english_dictionary!
+# TODO: At least the translations for 'Please', 'translate', 'this' and 'sentence'
+# NOTE: If you use the infinitive for 'translate' no problem!
+# NOTE: This is only a good showcase why translation of natural languages
+#       is a tough problem
+# ------------ add more key-value pairs to 'english_dictionary' here ----------
+
+# We can split a string into a list of strings with 'split'
+words_of_sentence = sentence.split(" ")
+puts "The sentence split into words looks like this"
+p words_of_sentence
+# We can iterate over a list with 'collect' and return a new list
+# NOTE: This time, the curly braces indicate a block
+translated_words = words_of_sentence.collect {|word| english_dictionary[word]}
+puts "The list of translated words looks like this"
+p translated_words
+# We can combine a list of strings into one string with 'join'
+puts "The final sentence looks like this"
+puts translated_words.join(" ")
+
+puts # ------------ print empty line -------------
+
+# Now why do we introduce symbols and hashes in one example?
+# Because symbols are often used as keys for a hash
+# Remember that we said, use symbols when you talk about "Who" instead of"What"?
+
+hash_with_symbols = {
+  :a_symbol => "is just perfect",
+  :as_a_key => "for a value"
+}
+# By the way: You can store any object in a hash, not just Strings
+hash_with_symbols[:a_number] = 42
+hash_with_symbols[:a_list] = ["one", 2, 3.0, :four]
+hash_with_symbols[:another_hash] = {:foo => :bar}
+puts "The 'hash_with_symbols' contains"
+p hash_with_symbols
+
+
+# TODO: create a command line form for address details
+address_details = {
+  :name => nil,
+  :street => nil,
+  :zip_code => nil,
+  :city => nil,
+}
+
+puts "Your address please!"
+address_details.keys.each do |key|
+  puts "Please type in your #{key.capitalize}"
+  # TODO: access the user input with 'gets'
+  # TODO: save the input the corresponding key in the 'address_details'
+  # NOTE: we iterate over all keys in this loop, so you've got the 'key' ;)
+end
+
+puts # ------------ print empty line -------------
+puts "Great! We will send your package to:"
+address_details.each do |key, value|
+  puts "#{key.capitalize}: #{value}"
+end
+
